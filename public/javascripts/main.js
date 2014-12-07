78ff68e4ee628ccef9c293c276014dd927a0563a
tfchang <tingfchang@gmail.com>
2014-12-06 20:48:57 -0800

    Display photos for each place in a separate box. Fade in/out based on time intervals

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,25 +1,61 @@
+// Instagram client_id
+const clientID = '587253991bb24378bae6c7f27025c9ae';
+
+// Nine places at most, up to ten pictures for each place
+const maxPlace = 9;
+const maxPic = 10;
+var places = []; 
 var pictures = [];
 
-function getPictures() {
-  const clientID = '587253991bb24378bae6c7f27025c9ae';
+function setPlaces() {
+  places[0] = {name: "Metrotown"};
+  places[1] = {name: "Chapters (Robson & Howe)"};
+  places[2] = {name: "Queen Elizabeth Park"};
+
+  places[0].lat = 49.2279;
+  places[1].lat = 49.2822;
+  places[2].lat = 49.2431;
+
+  places[0].lng = -122.9993;
+  places[1].lng = -123.1209;
+  places[2].lng = -123.1110;
+};
+
+function getPictures(getStr, placeIndex) {
+  $.getJSON(getStr + '&callback=?', function(pics) {
+    for (var i = 0; i < maxPic; i++) {    
+      pictures[placeIndex][i] = pics.data[i].images.thumbnail.url;
+    };
+
+    var img = $('.picture').eq(placeIndex);
+    var index = 0;
+    var intv = setInterval( function() {
+      index ++;
+      if (index == maxPic - 1) {
+        clearInterval(intv);
+      };
+
+      img.attr('src', pictures[placeIndex][index]);
+      img.fadeIn(1000, function() {
+        setTimeout(function() { img.fadeOut(1000); }, 2000);
+      });
+    }, 4000);
+  });  
+};
+
+function loadPictures() { 
   var instaURL = 'https://api.instagram.com/v1/media/search?';
-  var latlngStr = 'lat=49.2279&lng=-122.9993&';
   var clientStr = 'client_id=' + clientID;
 
-  const pic_limit = 10;  
-  $.getJSON(instaURL + latlngStr + clientStr + '&callback=?', function(pics) {
-    for (var i = 0; i < pic_limit; i++) {
-      pictures[i] = pics.data[i].images.thumbnail.url;
-    };
-    console.log('after for');
-
-    $('.picture').each( function(index, elem) {
-      console.log(pictures[index]);
-      $(elem).attr('src', pictures[index]);
-    });
-  });
+  for (var pIndex = 0; pIndex < 3; pIndex++) {
+    var place = places[pIndex];
+    var latlngStr = 'lat=' + String(place.lat) + '&lng=' + String(place.lng) + '&';
+    pictures[pIndex] = [];
+    getPictures(instaURL + latlngStr + clientStr, pIndex);
+  };
 };
 
 $(function() {
-  getPictures();
+  setPlaces();
+  loadPictures();
 });
