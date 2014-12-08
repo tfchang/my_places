f7030767537ddf0cf133d5c9c4599d69bab9c197
tfchang <tingfchang@gmail.com>
2014-12-08 09:05:33 -0800

    put setPlaces into a separate JS file

---
+++ public/javascripts/places.js
@@ -0,0 +1,33 @@
+var places = [];
+
+function setPlaces() {
+  places[0] = {name: "Metrotown"};
+  places[1] = {name: "Chapters (Robson & Howe)"};
+  places[2] = {name: "Queen Elizabeth Park"};
+  places[3] = {name: "Stanley Park"};
+  places[4] = {name: "Granville Island"};
+  places[5] = {name: "UBC"};
+  places[6] = {name: "SFU"};
+  places[7] = {name: "Westminster Pier Park"};
+  places[8] = {name: "Aberdeen Centre"};
+
+  places[0].lat = 49.2279;
+  places[1].lat = 49.2822;
+  places[2].lat = 49.2431;
+  places[3].lat = 49.3075;
+  places[4].lat = 49.2724;
+  places[5].lat = 49.2608;
+  places[6].lat = 49.2783;
+  places[7].lat = 49.2028;
+  places[8].lat = 49.1838;
+
+  places[0].lng = -122.9993;
+  places[1].lng = -123.1209;
+  places[2].lng = -123.1110;
+  places[3].lng = -123.1412;
+  places[4].lng = -123.1340;
+  places[5].lng = -123.2460;
+  places[6].lng = -122.9199;
+  places[7].lng = -122.9060;
+  places[8].lng = -123.1337;
+};
