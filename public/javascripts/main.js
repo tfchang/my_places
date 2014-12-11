d73d477b3668105f5b263b6a9b946c7e55bf9d72
tfchang <tingfchang@gmail.com>
2014-12-11 08:23:53 -0800

    use SlideShow() function prototype instead of for loop to show pictures for each place

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -42,43 +42,81 @@ function getPictures() {
   };
 };
 
-function showPicture(picIndex, placeIndex) {
-  var img = $('.picture').eq(placeIndex);
-  var img_url = pictures[placeIndex][picIndex].smallURL;
-  pictTimeoutIDs[placeIndex] = [];
+// function showPicture(picIndex, placeIndex) {
+//   var img = $('.picture').eq(placeIndex);
+//   var img_url = pictures[placeIndex][picIndex].smallURL;
+//   pictTimeoutIDs[placeIndex] = [];
 
-  pictTimeoutIDs[placeIndex][picIndex] = setTimeout(function() {
-    img.attr('src', img_url);
-    img.data("place-index", placeIndex);
-    img.data("pic-index", picIndex);
+//   pictTimeoutIDs[placeIndex][picIndex] = setTimeout(function() {
+//     img.attr('src', img_url);
+//     img.data("place-index", placeIndex);
+//     img.data("pic-index", picIndex);
     
-    // console.log('fade in/out: ' + String(placeIndex) + ', ' + String(picIndex));
-    img.fadeIn(500).delay(3000).fadeOut(500);
-  }, picIndex * 4000);
+//     // console.log('fade in/out: ' + String(placeIndex) + ', ' + String(picIndex));
+//     img.fadeIn(500).delay(3000).fadeOut(500);
+//   }, picIndex * 4000);
+// };
+
+// function showPlacePics(placeIndex) {
+//   if (stopPictures) {
+//     setRandDefer.resolve();
+//     return; 
+//   }
+  
+//   gotPlaces[placeIndex].done( function() {
+//     var placePics = pictures[placeIndex];
+//     for (var picIndex = 0; picIndex < placePics.length; picIndex++) {    
+//       showPicture(picIndex, placeIndex);
+//     };
+    
+//     // Replace pictures in an infinite loop
+//     setTimeout( function() {
+//       showPlacePics(placeIndex);
+//     }, placePics.length * 4000 );
+//   });
+// };
+
+function SlideShow(placeIndex) {
+  this.placeIndex = placeIndex;
+  this.img = $('.picture').eq(placeIndex);
+  this.pictures = pictures[placeIndex];
 };
 
-function showPlacePics(placeIndex) {
-  if (stopPictures) {
-    setRandDefer.resolve();
-    return; 
-  }
+SlideShow.prototype.start = function() {
+  this.picIndex = 0;
+  this.next();
+};
+
+SlideShow.prototype.next = function() {
+  this.picIndex += 1;
+
+  this.img.attr('src', this.pictures[this.picIndex].smallURL);
+  this.img.data("place-index", this.placeIndex);
+  this.img.data("pic-index", this.picIndex);
+  this.img.fadeIn(500).delay(3000).fadeOut(500);
+
+  if(this.picIndex < this.pictures.length) {
+    setTimeout(function() { this.next() }.bind(this), 4000); 
+  };  
+};
+
+function showPlacePics(placeIndex, slideShows) {
+  // if (stopPictures) {
+  //   setRandDefer.resolve();
+  //   return; 
+  // }
   
   gotPlaces[placeIndex].done( function() {
-    var placePics = pictures[placeIndex];
-    for (var picIndex = 0; picIndex < placePics.length; picIndex++) {    
-      showPicture(picIndex, placeIndex);
-    };
-    
-    // Replace pictures in an infinite loop
-    setTimeout( function() {
-      showPlacePics(placeIndex);
-    }, placePics.length * 4000 );
+    slideShows[placeIndex] = new SlideShow(placeIndex);
+    slideShows[placeIndex].start();
   });
 };
 
-function showPictures() { 
-  for (var placeIndex = 0; placeIndex < maxPlace; placeIndex++) { 
-    showPlacePics(placeIndex);
+function showPictures() {
+  var slideShows = [];
+
+  for (var placeIndex = 0; placeIndex < maxPlace; placeIndex++) {
+    showPlacePics(placeIndex, slideShows);
   };
 };
 
